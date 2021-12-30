5505803a1c8662618b9a86fdc0bb67c284acf85d
Skyline-23 <bs2740@naver.com>
2021-12-30 22:07:56 +0900

    [UPDATE] update Methods and properties

--- RxLocalAuthentication/Classes/RxLocalAuthentication.swift
+++ RxLocalAuthentication/Classes/RxLocalAuthentication.swift
@@ -34,4 +34,96 @@ public class RxLAContext {
             return Disposables.create()
         }
     }
+    
+    public func invalidate() {
+        self.context.invalidate()
+    }
+    
+    public func setCredential(_ credential: Data?, type: LACredentialType) -> Bool {
+        self.context.setCredential(credential, type: type)
+    }
+    
+    public func isCredentialSet(_ type: LACredentialType) -> Bool {
+        self.context.isCredentialSet(type)
+    }
+    
+    public func evaluateAccessControl(_ accessControl: SecAccessControl, operation: LAAccessControlOperation, localizedReason: String) -> Single<Bool> {
+        Single<Bool>.create { single in
+            self.context.evaluateAccessControl(accessControl, operation: operation, localizedReason: localizedReason) { result, error in
+                if let error = error {
+                    single(.failure(error))
+                } else {
+                    single(.success(result))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    public var localizedFallbackTitle: String? {
+        get {
+            self.context.localizedFallbackTitle
+        }
+        
+        set(value) {
+            self.context.localizedFallbackTitle = value
+        }
+    }
+    
+    @available(iOS 10.0, *)
+    public var localizedCancelTitle: String? {
+        get {
+            self.context.localizedCancelTitle
+        }
+        
+        set(value) {
+            self.context.localizedCancelTitle = value
+        }
+    }
+    
+    open var evaluatedPolicyDomainState: Data? {
+        get {
+            self.context.evaluatedPolicyDomainState
+        }
+    }
+    
+    public var touchIDAuthenticationAllowableReuseDuration: TimeInterval {
+        get {
+            self.context.touchIDAuthenticationAllowableReuseDuration
+        }
+        
+        set(value) {
+            self.context.touchIDAuthenticationAllowableReuseDuration = value
+        }
+    }
+    
+    @available(iOS 11.0, *)
+    public var localizedReason: String {
+        get {
+            self.context.localizedReason
+        }
+        
+        set(value) {
+            self.context.localizedReason = value
+        }
+    }
+    
+    @available(iOS 11.0, *)
+    public var interactionNotAllowed: Bool {
+        get {
+            self.context.interactionNotAllowed
+        }
+        
+        set(value) {
+            self.context.interactionNotAllowed = value
+        }
+    }
+    
+    @available(iOS 11.0, *)
+    public var biometryType: LABiometryType {
+        get {
+            self.context.biometryType
+        }
+    }
+
 }
