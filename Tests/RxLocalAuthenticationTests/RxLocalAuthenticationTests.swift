00d93208f67fa58d77f95ba3a9adff348f1cb52b
Skyline-23 <bs2740@naver.com>
2021-12-30 23:20:14 +0900

    [UPDATE] update path

---
+++ Tests/RxLocalAuthenticationTests/RxLocalAuthenticationTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RxLocalAuthentication
+
+final class RxLocalAuthenticationTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RxLocalAuthentication().text, "Hello, World!")
+    }
+}
